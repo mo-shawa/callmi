28d83ccb0500d140a3c3e88861defdf84b374523
Mahmoud Shawa <mahmoud@shawa.dev>
2023-11-06 22:42:13 +0300

    posting to sheets

--- src/actions.ts
+++ src/actions.ts
@@ -1,6 +1,8 @@
 'use server'
+import { google } from 'googleapis'
 
 export async function subscribe(data: FormData) {
+  'use server'
   try {
     const email = data.get('email')
 
@@ -8,14 +10,45 @@ export async function subscribe(data: FormData) {
       throw new Error('Email is required')
     }
 
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      },
+      scopes: [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/spreadsheets',
+      ],
+    })
+
+    const sheets = google.sheets({ version: 'v4', auth })
+
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: 'Sheet1!A1',
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [[email]],
+      },
+    })
+
+    if (response.status !== 200) {
+      throw new Error('Something went wrong with Google Sheets')
+    }
+
+    console.log({ response })
+
     return {
       status: 200,
       body: { message: 'Success!' },
     }
-  } catch (error) {
+  } catch (error: any) {
+    const e: Error = error
+
     return {
       status: 400,
-      body: { error: 'Something went wrong.. Please try again in a moment.' },
+      body: { error: e.message },
     }
   }
 }
