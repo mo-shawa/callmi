a36561859a0b45c9d484fdb54822de4a094c1c58
Mahmoud Shawa <mahmoud@shawa.dev>
2023-11-06 18:16:58 +0300

    add form submitted state

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react'
-
+import Image from 'next/image'
 export default function EmailForm() {
   const [email, setEmail] = useState('')
+  const [submitted, setSubmitted] = useState(false)
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     console.log(email)
+    setSubmitted(true)
   }
+
+  if (submitted)
+    return (
+      <div className="flex flex-col items-center justify-center gap-4">
+        <Image src="/logo.svg" alt="logo" height={96} width={96} />
+        <p className="text-lg font-light">Thanks for signing up!</p>
+      </div>
+    )
 
   return (
     <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
@@ -15,6 +25,7 @@ export default function EmailForm() {
         className="border-dark rounded-3xl border p-4"
         type="email"
         placeholder="your@email.com"
+        required
         id="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
