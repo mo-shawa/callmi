97b4ab74fdae2ceb1a2d14162c08564ac20b1ccd
Mahmoud Shawa <mahmoud@shawa.dev>
2023-11-06 23:19:18 +0300

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,18 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Callmi',
-  description: 'Callmi - chat for cash',
+  title: 'Callmi ',
+  description:
+    'Callmi - Ready to value your expertise? Start monetizing your knowledge today. Join callmi and turn your passion into profit.',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
